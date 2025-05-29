3520c9e5f9ff99c0f76ac6bb8650dee4c9bbc464
Thomas Eby <tgeby@icloud.com>
2025-05-29 17:35:57 -0400

    Initial files. Xcode watchOS app with companion IOS app.

---
+++ TimerTool/TimerTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimerTool
+//
+//  Created by Thomas Eby on 5/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world on iPhone!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimerTool/TimerTool/TimerToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerToolApp.swift
+//  TimerTool
+//
+//  Created by Thomas Eby on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
