09b5e8952b4c1c4dc237a280e4bb5978cc7fb54f
Thomas Eby <tgeby@icloud.com>
2025-05-30 20:09:31 -0400

    Created a menu to switch to a EnterIntervalView

---
+++ TimerTool/TimerTool/EnterIntervalView.swift
@@ -0,0 +1,18 @@
+//
+//  EnterIntervalView.swift
+//  TimerTool
+//
+//  Created by Thomas Eby on 5/30/25.
+//
+
+import SwiftUI
+
+struct EnterIntervalView: View {
+    var body: some View {
+        Text("Hello, from the enter interval view")
+    }
+}
+
+#Preview {
+    EnterIntervalView()
+}
---
+++ TimerTool/TimerTool/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  TimerTool
+//
+//  Created by Thomas Eby on 5/30/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Hello, from the test view")
+    }
+}
+
+#Preview {
+    TestView()
+}
