f8de459526031104f2c16ceba1f0483101f48e11
Thomas Eby <tgeby@icloud.com>
2025-05-31 13:23:43 -0400

    #2, #3 Implemented the watchOS app's main menu.

--- TimerTool/TimerTool Watch App/ContentView.swift
+++ TimerTool/TimerTool Watch App/ContentView.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack {
-            NavigationView {
-                VStack {
-                    NavigationLink(destination: LibraryView()) {
-                        Text("Show stored interval sequences")
+        NavigationView {
+            ZStack {
+                Color(red: 80/255, green: 200/255, blue: 120/255)
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 10) {
+                    NavigationLink(destination: ExerciseView()) {
+                        Text("Use a timer")
+                            .frame(width: 150, height: 60)
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
                     }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    NavigationLink(destination: LibraryView()) {
+                        Text("Manage library")
+                            .frame(width: 150, height: 60)
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
+            .frame(maxHeight: .infinity)
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ TimerTool/TimerTool Watch App/ExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseView.swift
+//  TimerTool Watch App
+//
+//  Created by Thomas Eby on 5/31/25.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseView()
+}
